2058d0b10f97bd6d2ed637527c41272eafc99ad8
fangon.274255 <fangon.274255@ANNEX-PC21>
2025-09-19 18:57:47 +0800

    Add project files.

---
+++ ThreadPriority/MyThreadClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadPriorityDemo
+{
+    public class MyThreadClass
+    {
+        public static void Thread1()
+        {
+            Thread thread = Thread.CurrentThread;
+            for (int loopCount = 0; loopCount < 2; loopCount++)
+            {
+                Console.WriteLine("Name of Thread: " + thread.Name + " = " + loopCount);
+                Thread.Sleep(500);
+            }
+        }
+
+        public static void Thread2()
+        {
+            Thread thread = Thread.CurrentThread;
+            for (int loopCount = 0; loopCount < 6; loopCount++)
+            {
+                Console.WriteLine("Name of Thread: " + thread.Name + " = " + loopCount);
+                Thread.Sleep(1500);
+            }
+        }
+    }
+}
---
+++ ThreadPriority/frmTrackThread.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ThreadPriorityDemo;
+
+namespace ThreadPriority
+{
+    public partial class frmTrackThread : Form
+    {
+        
+        public frmTrackThread()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("THREAD STARTS");
+            Thread threadA = new Thread(MyThreadClass.Thread1) { Name = "Thread A" };
+            Thread threadB = new Thread(MyThreadClass.Thread2) { Name = "Thread B" };
+            Thread threadC = new Thread(MyThreadClass.Thread1) { Name = "Thread C" };
+            Thread threadD = new Thread(MyThreadClass.Thread2) { Name = "Thread D" };
+
+            threadA.Priority = System.Threading.ThreadPriority.Highest;
+            threadB.Priority = System.Threading.ThreadPriority.Normal;
+            threadC.Priority = System.Threading.ThreadPriority.AboveNormal;
+            threadD.Priority = System.Threading.ThreadPriority.BelowNormal;
+
+            threadA.Start();
+            threadB.Start();
+            threadC.Start();
+            threadD.Start();
+
+            threadA.Join();
+            threadB.Join();
+            threadC.Join();
+            threadD.Join();
+
+            Console.WriteLine("END OF THREAD");
+
+            label1.Text = "END OF THREAD";
+        }
+    }
+}
